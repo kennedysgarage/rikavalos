485f71e407df83db22c3e3ed20a4ddaaab808ea9
Kennedy <kennedy@kennedys-mbp.lan>
2019-10-01 09:07:11 -0700

    init commit

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title><?php if($title == true) : ?><?php echo $title ?> | <?php endif; ?>Rik Avalos</title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+  <link rel="shortcut icon" href="/favicon.png">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/assets/css/reset.css">
+  <link rel="stylesheet" href="/assets/css/style.css">
+
+  <script src="/assets/js/modernizr.min.js"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123671864-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-123671864-2');
+  </script>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php  $title = ''; include 'includes/header.php'; ?>
+
+<div class="container">
+
+  <section class="page">
+    <h1>C.M. Kennedy</h1>
+    <div class="clear"></div>
+    <div class="row">
+      <div class="six columns">
+        <p class="to">Vice President of Talent and Culture<br/>
+        Mr. Rick Avalos<br/>
+        Outdoorsy<br/>
+        San Francisco, CA</p><br/>
+      </div><!--/col-->
+      <div class="six columns">
+        <p class="date">October 1st, 2019</p>
+      </div><!--/col-->
+    </div><!--/row-->
+    <p>Dear Rik,</p>
+    <p>My name is <a href="http://kennedysgarage.com/">Kennedy</a> and last year I purchased Sapo, a 2003 Volkswagen Eurovan Camper. I had the chance to travel around the United States, where I explored new places and met interesting people along the way. When I wasn't on the road, I listed <a href="https://www.outdoorsy.com/rv/100943">Sapo</a> on Outdoorsy as a way to make extra money. So far,  I have made $6,138.00. I love the opportunities that Outfoorsy provides.</p>
+    <p>At this period you might be asking yourself why would someone purchased your expired domain. Ahh, great question. I am a senior UX designer, Xoogler, and I am currently freelancing while working on my own projects. I am now looking to move back in-house at a startup. I love what Outdoorsy is doing, and I want to be apart of it.</p>
+    <p>Sincerely,<br/>
+    <img src="/assets/img/signature.svg" class="signature"/><br/>
+    C.M. Kennedy</p><br/>
+    <p class="ps">P.S. Don't worry if things don't work out between us, I am more than happy to transfer this domain to you free of cost.</p>
+    <img src="/assets/img/logo.svg" class="logo"/>
+  </section>
+
+  <section>
+    <div class="row">
+      <div class="twelve columns">
+        <hr/>
+      </div><!--/col-->
+    </div><!--/row-->
+  </section>
+
+  <section class="aword">
+    <span class="eyebrow">A note from</span>
+    <h2 class="title">The Desk of Kennedy</h2>
+    <div class="vid">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Cm9WtKK7tf8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div><!--/vid-->
+    <div class="row">
+       <div class="six columns">
+        <p>I am a senior UX designer, Xoogler, and is currently freelancing while working on his own <a href="https://fistatus.com/">fintech project</a>. Ano I am now looking to move back in-house at a startup. I am generally company and stage agnostic. I am looking for a company that cares strongly about its customers, brand, and visual identity.</p>
+      </div><!--/col-->
+       <div class="six columns">
+         <p>Most recently, I was full-time for 2.5 years at WeWork as a Design Director. I had the opportunity to manage a design team that worked on digital, physical, and industrial experiences where I collaborated closely with Adam Neumann and Tomer Sharon.</p>
+      </div><!--/col-->
+    </div><!--/row-->
+  </section>
+
+  <section>
+    <div class="row">
+      <div class="twelve columns">
+        <hr/>
+      </div><!--/col-->
+    </div><!--/row-->
+
+    </div>
+  </section>
+
+  <section>
+    <div class="contact">
+    <div class="row">
+       <div class="three columns">
+         <img src="/assets/img/face.png"/>
+      </div><!--/col-->
+       <div class="nine columns">
+         <span class="eyebrow">Say hi</span>
+         <h2 class="title">Let’s Chat</h2>
+         <p>I know I can add a lot of value to Outdoorsy. I am an experienced customer, lived and traveled in their van, and clearly has a passion for the industry.</p>
+         <p>Get in touch with me at <a href="mailto:kennedy@byfreeagency.com">kennedy@byfreeagency.com</a>.</p>
+      </div><!--/col-->
+    </div><!--/row-->
+    </div><!--/contact-->
+  </section>
+
+</div><!--/container-->
+
+<?php include("includes/footer.php"); ?>
