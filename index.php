b1b43b1e5b9b625cd31a742b7df769da41bf5369
Kennedy <kennedy@kennedys-mbp.lan>
2019-10-01 14:33:01 -0700

    fixed more spelling and grammer

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <div class="row">
       <div class="six columns">
         <p class="to">Vice President of Talent and Culture<br/>
-        Mr. Rick Avalos<br/>
+        Mr. Rik Avalos<br/>
         Outdoorsy<br/>
         San Francisco, CA</p><br/>
       </div><!--/col-->
@@ -17,8 +17,8 @@
       </div><!--/col-->
     </div><!--/row-->
     <p>Dear Rik,</p>
-    <p>My name is <a href="http://kennedysgarage.com/">Kennedy</a> and last year I purchased Sapo, a 2003 Volkswagen Eurovan Camper. I had the chance to travel around the United States, where I explored new places and met interesting people along the way. When I wasn't on the road, I listed <a href="https://www.outdoorsy.com/rv/100943">Sapo</a> on Outdoorsy as a way to make extra money. So far,  I have made $6,138.00. I love the opportunities that Outfoorsy provides.</p>
-    <p>At this period you might be asking yourself why would someone purchased your expired domain. Ahh, great question. I am a senior UX product designer who is currently freelancing. I am now looking to move back in-house at a startup. I love what Outdoorsy is doing, and I want to be apart of it.</p>
+    <p>My name is <a href="http://kennedysgarage.com/">Kennedy</a> and last year I purchased Sapo, a 2003 Volkswagen Eurovan Camper. I had the chance to travel around the United States, where I explored new places and met interesting people along the way. When I wasn't on the road, I listed <a href="https://www.outdoorsy.com/rv/100943">Sapo</a> on Outdoorsy as a way to make extra money. So far,  I have made $6,138.00. I love the opportunities that Outdoorsy provides.</p>
+    <p>At this point you might be asking yourself, "why would someone purchase my expired domain?" Ahh, great question. I am a senior UX product designer who is currently freelancing. I am now looking to move back in-house at a startup. I love what Outdoorsy is doing, and I want to be a part of it.</p>
     <p>Sincerely,<br/>
     <img src="/assets/img/signature.svg" class="signature"/><br/>
     C.M. Kennedy</p><br/>
@@ -42,7 +42,7 @@
     </div><!--/vid-->
     <div class="row">
        <div class="six columns">
-        <p>I am a senior UX designer, Xoogler, and I am currently freelancing while working on my own <a href="https://www.vanimal.co/">van side project</a>. I am now looking to move back in-house at a startup. I am generally company and stage agnostic. I am looking for a company that cares strongly about its customers, brand, and visual identity.</p>
+        <p>I am a senior UX designer, Xoogler, and I am currently freelancing while working on my own <a href="https://www.vanimal.co/">van side project</a>. I am now looking to move back in-house at a startup. I am generally company- and stage-agnostic. I am looking for a company that cares strongly about its customers, brand, and visual identity.</p>
       </div><!--/col-->
        <div class="six columns">
          <p>Most recently, I was full-time for 2.5 years at WeWork as a Design Director. I had the opportunity to manage a design team that worked on digital, physical, and industrial experiences where I collaborated closely with Adam Neumann and Tomer Sharon.</p>
@@ -69,7 +69,7 @@
        <div class="nine columns">
          <span class="eyebrow">Say hi</span>
          <h2 class="title">Let’s Chat</h2>
-         <p>I know I can add a lot of value to Outdoorsy. I am an experienced customer, traveled in a van, and clearly has a passion for the industry.</p>
+         <p>I know I can add a lot of value to Outdoorsy. I am an experienced customer, I've traveled the country in a van, and I clearly have a passion for the industry.</p>
          <p>Get in touch with me at <a href="mailto:kennedy@byfreeagency.com">kennedy@byfreeagency.com</a>.</p>
       </div><!--/col-->
     </div><!--/row-->
